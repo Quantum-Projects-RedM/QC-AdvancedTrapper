9cf694836f720aa5718b0bfc526e5170f877d78b
Artmines <96462463+Artmines@users.noreply.github.com>
2024-11-19 21:44:53 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game 'rdr3'
 
-author 'Pablo Enterprises'
+author 'Pablo Enterprises and RexShack'
 description 'qc-advanced-trapper'
 
 shared_scripts {
@@ -12,10 +12,10 @@ shared_scripts {
     '@PolyZone/EntityZone.lua',
     '@PolyZone/CircleZone.lua',
     '@PolyZone/ComboZone.lua',
-    'config.lua',
     'locales/en.lua',
     'locales/*.lua',
     '@ox_lib/init.lua',
+    'shared/*.lua'
 }
 
 client_scripts {
